cafd48f8a2e8a898185131c5e7ec97c6a59d5acf
Deyvison Iury <iurydeyvison@gmail.com>
2024-07-20 13:42:27 -0300

    new: Resposta do segundo desafio

---
+++ desafios/respostas/02-booleanos.ts
@@ -0,0 +1,31 @@
+// Boleanos
+
+const user = {
+  name: 'Diego Fernandes',
+  height: 190,
+  hasTicket: true,
+}
+
+const necessaryHeight = 130
+
+const currentHour = new Date().getHours()
+
+const isParkOpen = currentHour > 9 && currentHour < 18
+
+if (!isParkOpen) {
+  throw new Error('O parque está fechado!')
+}
+
+const userHasTicket = user.hasTicket
+
+if (!userHasTicket) {
+  throw new Error('O Diego não possui um bilhete para entrar no parque!')
+}
+
+const isUserTallEnoughForToy = user.height > necessaryHeight
+
+if (!isUserTallEnoughForToy) {
+  throw new Error('O Diego não pode entrar no brinquedo!')
+}
+
+console.log('O Diego se divertiu muito! :)')
