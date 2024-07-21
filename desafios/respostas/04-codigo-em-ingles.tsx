4b06e670963a23a7e9a8e8a0e4c77cc5ad445c83
Deyvison Iury <iurydeyvison@gmail.com>
2024-07-21 15:46:23 -0300

    new: Resposta do quarto desafio

---
+++ desafios/respostas/04-codigo-em-ingles.tsx
@@ -0,0 +1,43 @@
+// Código em inglês 
+import { useState } from "react"
+
+interface Product {
+  title: string;
+  price: string;
+}
+
+const productList = [
+  {
+    title: 'Macarrão',
+    price: 'R$ 25,00'
+  },
+  {
+    title: 'Hamburger',
+    price: 'R$ 30,00'
+  }
+]
+
+export function ListProduct() {
+  const [FilteredProducts, setFilteredProducts] = useState<Product[]>([])
+
+  function searchProduct(search: string) {
+    const filtered = productList.filter(product => product.title.includes(search))
+
+    setFilteredProducts(filtered)
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => searchProduct(e.target.value)} />
+
+      {FilteredProducts.map(product => (
+        <div>
+          <p>{product.title}</p>
+          <p>{product.price}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+
