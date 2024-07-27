5d18ece444469dc6a53c082255fbba80d11c63c2
Deyvison Iury <iurydeyvison@gmail.com>
2024-07-27 14:37:28 -0300

    new: respostas do sétimo desafio

---
+++ desafios/respostas/07-numeros-magicos.js
@@ -0,0 +1,9 @@
+function lookForUpdades() {}
+
+const INTERVAL_30_MINUTER  = 60 * 30 * 1000; 
+
+setInterval(lookForUpdades, REFRESH_RATE)
+
+function calculateDiscount(priceInCents, discountPercentage) {
+
+}
